7479a464630e617c0a496147f609336f0e5cc3b4
Ahmad Reza Enshaee <hajahmad.reza1998@gmail.com>
2017-09-02 16:16:08 +0430

    Initial commit

---
+++ app/src/main/java/ir/enshaee/karpardaz/presenter/database/SQLKeys.java
@@ -0,0 +1,14 @@
+package ir.enshaee.karpardaz.presenter.database;
+
+
+public interface SQLKeys {
+
+      String TABLE_TOJIBI = "Tojibi";
+      String TABLE_TANKHAH = "Tankhah";
+      
+      String COLUMN_ID = "Id";
+      String COLUMN_DATE = "Date";
+      String COLUMN_QUANTITY = "Quantity";
+      String COLUMN_SUBJECT = "Subject";
+      
+}
